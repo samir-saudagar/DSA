e1f8c2f1c3f4766f23f08c5fffbbd0b9d98a9227
samir saudagar <samir.y.saudagar@gmail.com>
2025-07-24 22:50:56 +0530

    Update string_valid_number.cpp

--- string/string_valid_number.cpp
+++ string/string_valid_number.cpp
@@ -22,7 +22,8 @@ public:
                 plus_minus_count++;
             } else if(s[i] == '.') {
                 if(eE_seen || dot_seen) return false;
-                if( i == n - 1 && !digit_seen) return false;
+                if ((i == n - 1 && !digit_seen) || 
+                    (i == 0 && !isdigit(s[i + 1])))  return false;
                 dot_seen = true;
             } else if(s[i] == 'e' || s[i] == 'E') {
                 if(eE_seen || !digit_seen || i == n - 1) return false;
