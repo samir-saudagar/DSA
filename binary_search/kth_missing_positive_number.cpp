b4e65022076ac6d2b258181f5e2f8bf226ebcfb4
samir-saudagar <samir.y.saudagar@gmail.com>
2025-08-08 23:53:10 +0530

    solve: kth missing positive number

---
+++ binary_search/kth_missing_positive_number.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/kth-missing-positive-number?envType=problem-list-v2&envId=binary-search
+// n is size of arr
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) { // Time: O(log n), Space: O(1)
+        int i = 0, j = arr.size() - 1;
+
+        while(i <= j) {
+            int mid = i + (j - i) / 2;
+            int missing_count = arr[mid] - (mid + 1);
+            if(missing_count >= k) j = mid -1;
+            else i = mid + 1;
+        }
+        return i + k;
+    }
+};
+
+//Note: obervation:
+// The number of missing positive integers before the mid index is equal to arr[mid] - (mid + 1)
