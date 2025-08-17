2fbbed11921f52abf1b66ac4cba1a198f83b5ae7
samir-saudagar <samir.y.saudagar@gmail.com>
2025-08-17 12:51:21 +0530

    Added better solution

--- Array/pascals_triangle.cpp
+++ Array/pascals_triangle.cpp
@@ -1,8 +1,8 @@
-// https://leetcode.com/problems/pascals-triangle?envType=daily-question&envId=2025-08-01
-// n is the number of rows in Pascal's Triangle
+// https://leetcode.com/problems/pascals-triangle
+
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) { // O(n^2) time, O(n^2) space
+    vector<vector<int>> generate(int numRows) { // Time: O(numRows^2), Space: O(numRows^2)
         vector<vector<int>> ans;
         for(int i = 0; i < numRows; i++) {
             vector<int> temp(i + 1, 1);
@@ -12,5 +12,21 @@ public:
             }
         }
         return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) { // Time: O(numRows^2), Space: O(numRows^2)
+        vector<vector<int>> pascal(numRows);
+
+        for (int i = 0; i < numRows; i++) {
+            pascal[i].resize(i + 1, 1); 
+            for (int j = 1; j < i; j++) {
+                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+            }
+        }
+
+        return pascal;
     }
 };
